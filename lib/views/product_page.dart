0aac5902842108d890b1c6f395a8beabc572f46c
Sonos99-dev <dev.sonos99@gmail.com>
2026-01-31 08:15:30 +0900

    fix: 거스름돈이 표시되지 않는 버그수정

--- lib/views/product_page.dart
+++ lib/views/product_page.dart
@@ -544,7 +544,7 @@ class _ProductPageState extends State<ProductPage> {
                                     onTap: () {
                                       _showCashInputPad(context, (totalAmount) {
                                         setDialogState(() {
-                                          cashController.text = AppFormat.won(totalAmount).toString();
+                                          cashController.text = totalAmount.toString();
                                         });
                                       });
                                     },
