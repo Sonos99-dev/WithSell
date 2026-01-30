b7d419aa5369e71cd8b859cee77e08dbf24f597f
Sonos99-dev <dev.sonos99@gmail.com>
2026-01-30 17:13:03 +0900

    fix: 뒤로가기를 두번 빠르게 클릭해야 앱이 종료되도록 변경

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:project/viewmodels/sales_history_view_model.dart';
 import 'package:project/views/admin_page.dart';
 import 'package:project/views/app_color.dart';
@@ -19,9 +20,9 @@ class _MainScreenState extends State<MainScreen> {
   // 4개의 메뉴 리스트
   final List<Widget> _pages = [
     const ProductPage(), // 1번: 기존 상품 페이지
-    const SalesHistoryPage(),      // 2번: 검색 (추후 분리)
-    const Center(child: Text('장바구니')),    // 3번: 장바구니 (추후 분리)
-    const AdminPage(),  // 4번: 마이페이지 (추후 분리)
+    const SalesHistoryPage(), // 2번: 검색 (추후 분리)
+    const Center(child: Text('장바구니')), // 3번: 장바구니 (추후 분리)
+    const AdminPage(), // 4번: 마이페이지 (추후 분리)
   ];
 
   void _onItemTapped(int index) {
@@ -36,59 +37,86 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-          splashColor: Colors.white.withOpacity(0.1),
-          highlightColor: Colors.transparent,
-          hoverColor: Colors.transparent,
-        ),
-        child: BottomNavigationBar(
-          backgroundColor: AppColors.mainColor,
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-          selectedItemColor: Colors.white,
-          selectedLabelStyle: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 14,
-            shadows: [
-              Shadow(
-                offset: Offset(0, 1),
-                blurRadius: 4,
-                color: Color(0x55000000),
-              ),
-            ],
-          ),
-          selectedIconTheme: const IconThemeData(
-            size: 35,
-          ),
+    DateTime? _lastBackPressed;
 
-          unselectedItemColor: Colors.white54,
-          unselectedLabelStyle: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 12,
-            shadows: [
-              Shadow(
-                offset: Offset(0, 1),
-                blurRadius: 4,
-                color: Color(0x55000000),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        if (didPop) return;
+
+        final now = DateTime.now();
+        final last = _lastBackPressed;
+
+        if (last == null || now.difference(last) > const Duration(seconds: 2)) {
+          _lastBackPressed = now;
+
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(const SnackBar(content: Text('한 번 더 누르면 앱이 종료됩니다')));
+          return;
+        }
+        SystemNavigator.pop();
+      },
+      child: Scaffold(
+        body: IndexedStack(index: _selectedIndex, children: _pages),
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            splashColor: Colors.white.withOpacity(0.1),
+            highlightColor: Colors.transparent,
+            hoverColor: Colors.transparent,
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: AppColors.mainColor,
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            selectedItemColor: Colors.white,
+            selectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+              shadows: [
+                Shadow(
+                  offset: Offset(0, 1),
+                  blurRadius: 4,
+                  color: Color(0x55000000),
+                ),
+              ],
+            ),
+            selectedIconTheme: const IconThemeData(size: 35),
+
+            unselectedItemColor: Colors.white54,
+            unselectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+              shadows: [
+                Shadow(
+                  offset: Offset(0, 1),
+                  blurRadius: 4,
+                  color: Color(0x55000000),
+                ),
+              ],
+            ),
+            unselectedIconTheme: const IconThemeData(size: 24),
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list_outlined),
+                label: '상품 목록',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.receipt_long),
+                label: '판매 내역',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calculate_rounded),
+                label: '정산',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.manage_accounts),
+                label: '관리자 설정',
               ),
             ],
           ),
-          unselectedIconTheme: const IconThemeData(
-            size: 24,
-          ),
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.list_outlined), label: '상품 목록'),
-            BottomNavigationBarItem(icon: Icon(Icons.receipt_long), label: '판매 내역'),
-            BottomNavigationBarItem(icon: Icon(Icons.calculate_rounded), label: '정산'),
-            BottomNavigationBarItem(icon: Icon(Icons.manage_accounts), label: '관리자 설정'),
-          ],
         ),
       ),
     );
